454a0e6d37989d7df091c780c9baa77d5420aff0
Saffia N <saffia.yniro@hotmail.com>
2021-02-15 23:32:25 +0000

    Camera position bug but pan and tilt works

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,50 +11,26 @@
 
 #include "shader.h"
 #include "Mesh.h"
+#include "Camera.h"
+#include "Window.h"
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
 float toRadians(float deg);
 void createGrid();
 
-const int WIDTH = 800, HEIGHT = 600;
+const int WIDTH = 1024, HEIGHT = 768;
 std::vector<Mesh*> meshList;
+Camera camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), 0.0f, 0.0f, 0.05f, 1.0f); // Initialize camera
+Window window;
 
 int main(int argc, char* argv[])
 {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Learn OpenGL", NULL, NULL);
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-
-	glfwMakeContextCurrent(window);
-
-	GLenum error = glewInit();
-	if (error != GLEW_OK)
-	{
-		printf("Error: %s", glewGetErrorString(error));
-		glfwDestroyWindow(window);
-		glfwTerminate();
-		return 1;
-	}
+	window = Window(WIDTH, HEIGHT);
+	window.initialise();
 
 	glEnable(GL_DEPTH_TEST);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
-	glViewport(0, 0, WIDTH, HEIGHT);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-
 
 	// Creating grid
 	createGrid();
@@ -64,45 +40,46 @@ int main(int argc, char* argv[])
 	projection = glm::perspective(45.0f, (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
 
 
-	while (!glfwWindowShouldClose(window))
+	while (!window.getShouldClose())
 	{
-		// manage input
-		processInput(window);
 
 		// rendering commands
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		// Camera movement
+		camera.pan(window.getKeys(), window.getDeltaX());
+		camera.tilt(window.getKeys(), window.getDeltaY());
+
 		gridShader.use();
 
+		// Model matrix
 		glm::mat4 model(1.0f);
 		model = glm::scale(model, glm::vec3(8.0f, 8.0f, 1.0f));
 		model = glm::translate(model, glm::vec3(-0.5f, 0.0f, -1.0f));
 		model = glm::rotate(model, toRadians(45), glm::vec3(1.0f, 0.0f, 0.0f));
+
+		// View matrix
+		glm::mat4 view(1.0f);
+		view = glm::translate(view, glm::vec3(-1.5f, 0.0f, -1.0f));
+		view = glm::rotate(view, toRadians(-85), glm::vec3(1.0f, 0.0f, 0.0f));
+		view = camera.calculateViewMatrix() * view;
+
+		// Connect matrices with shaders
 		gridShader.setMatrix4Float("model", &model);
 		gridShader.setMatrix4Float("projection", &projection);
+		gridShader.setMatrix4Float("view", &view);
 		meshList[0]->RenderMesh(GL_LINES);
 
 		gridShader.free();
 
 		//check and call events and sawp buffers
-		glfwSwapBuffers(window);
+		window.swapBuffers();
 		glfwPollEvents();
 	}
 
 	glfwTerminate();
 	return 0;
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
-
-void processInput(GLFWwindow* window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
 }
 
 float toRadians(float deg)
